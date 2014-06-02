c0a67df26622c3bf4b5edc7cd2c9ced92914bb38
Guillaume Seguin <guillaume@segu.in>
2014-06-02 23:59:42 +0200

    Improve stream.sh, allow setting parameters through external script

--- stream.sh
+++ stream.sh
@@ -7,12 +7,15 @@ ICECAST_MOUNT=""
 STREAM_TITLE=""
 STREAM_DESC=""
 STREAM_URL=""
+STREAM_GENRE=""
 
-./stream.py /dev/video* | ~/oggfwd/oggfwd $ICECAST_SERVER \
-                                          $ICECAST_PORT \
-                                          $ICECAST_PASSWORD \
-                                          $ICECAST_MOUNT \
-                                          -n $STREAM_TITLE \
-                                          -d $STREAM_TITLE \
-                                          -u $STREAM_URL \
-                                          -g $STREAM_DESC
+[ -f ./stream_params ] && . ./stream_params
+
+python2 stream.py /dev/video* | oggfwd $ICECAST_SERVER \
+                                       $ICECAST_PORT \
+                                       $ICECAST_PASSWORD \
+                                       $ICECAST_MOUNT \
+                                       -n "$STREAM_TITLE" \
+                                       -d "$STREAM_DESC" \
+                                       -u "$STREAM_URL" \
+                                       -g "$STREAM_GENRE"
