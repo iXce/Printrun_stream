0be8b6dc526723cddc6c2153fa6c1e9209dd00e3
Guillaume Seguin <guillaume@segu.in>
2014-06-02 23:25:35 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -55,83 +55,83 @@ TEMP_LOG_SIZE = 60
 LINEWIDTH = 10
 
 def make_temp_graph(temp_log):
-  first_log = temp_log[0]
-  tempnames = first_log.keys()
-  tempnames.sort()
-  temps = {}
-  for name in tempnames:
-    temps[name] = zip(*[entry[name] for entry in temp_log])
+    first_log = temp_log[0]
+    tempnames = first_log.keys()
+    tempnames.sort()
+    temps = {}
+    for name in tempnames:
+        temps[name] = zip(*[entry[name] for entry in temp_log])
 
-  pylab.rcParams['ytick.major.pad'] = '20'
-  fig = pylab.figure(figsize = (10, 10))
+    pylab.rcParams['ytick.major.pad'] = '20'
+    fig = pylab.figure(figsize = (10, 10))
 
-  fig.patch.set_facecolor('gray')
-  fig.patch.set_alpha(0.4)
+    fig.patch.set_facecolor('gray')
+    fig.patch.set_alpha(0.4)
 
-  ax = fig.add_subplot(1, 1, 1)
-  for child in ax.get_children():
-    if isinstance(child, mpl.spines.Spine):
-        child.set_color('#dddddd')
-  ax.tick_params(axis='y', colors='white')
+    ax = fig.add_subplot(1, 1, 1)
+    for child in ax.get_children():
+        if isinstance(child, mpl.spines.Spine):
+            child.set_color('#dddddd')
+    ax.tick_params(axis='y', colors='white')
 
-  ax.patch.set_facecolor('gray')
-  ax.patch.set_alpha(0.2)
+    ax.patch.set_facecolor('gray')
+    ax.patch.set_alpha(0.2)
 
-  pylab.xlim([0, TEMP_LOG_SIZE])
-  pylab.ylim([0, 300])
-  for name in tempnames:
-    thistemps = temps[name]
-    extra = (TEMP_LOG_SIZE - len(thistemps[0]))
-    idxs = range(extra, extra + len(thistemps[0]))
-    pylab.plot(idxs, thistemps[0], linewidth = LINEWIDTH)
-    pylab.plot(idxs, thistemps[1], '-', linewidth = LINEWIDTH)
+    pylab.xlim([0, TEMP_LOG_SIZE])
+    pylab.ylim([0, 300])
+    for name in tempnames:
+        thistemps = temps[name]
+        extra = (TEMP_LOG_SIZE - len(thistemps[0]))
+        idxs = range(extra, extra + len(thistemps[0]))
+        pylab.plot(idxs, thistemps[0], linewidth = LINEWIDTH)
+        pylab.plot(idxs, thistemps[1], '-', linewidth = LINEWIDTH)
 
-  ax.get_xaxis().set_ticks([])
-  for item in ax.get_xticklabels() + ax.get_yticklabels():
-    item.set_fontsize(30)
+    ax.get_xaxis().set_ticks([])
+    for item in ax.get_xticklabels() + ax.get_yticklabels():
+        item.set_fontsize(30)
 
-  svg = cStringIO.StringIO()
-  fig.savefig(svg, format = 'svg', bbox_inches = 'tight', pad_inches = 0.1, facecolor = fig.get_facecolor())
-  pylab.close(fig)
-  return svg.getvalue()
+    svg = cStringIO.StringIO()
+    fig.savefig(svg, format = 'svg', bbox_inches = 'tight', pad_inches = 0.1, facecolor = fig.get_facecolor())
+    pylab.close(fig)
+    return svg.getvalue()
 
 class StatusHandler(object):
 
-  def __init__(self):
-      self.temp_log = []
-      self.update_status()
+    def __init__(self):
+        self.temp_log = []
+        self.update_status()
 
-  def update_status(self):
-      try:
-          status = rpc.status()
-          status_text = ""
-          if status["filename"] is not None:
-              status_text += "Printing %s\n" % os.path.basename(status["filename"])
-          if status["eta"] is not None:
-              secondsremain, secondsestimate, progress = status["eta"]
-              status_text += "Est: %s of %s remaining\n" % (format_duration(secondsremain),
-                                                          format_duration(secondsestimate))
-          if status["temps"] is not None:
-              temps = status["temps"]
-              self.temp_log.append(temps)
-              if len(self.temp_log) > TEMP_LOG_SIZE:
-                self.temp_log.pop(0)
-              graphsvg = make_temp_graph(self.temp_log)
-              graphoverlay.set_property("data", graphsvg)
-              #print >> sys.stderr, len(self.temp_log), graphsvg
-              status_text += "Temps: H: %s/%s, B: %s/%s\n" % (temps["T"][0], temps["T"][1], temps["B"][0], temps["B"][1])
-          if status["z"] is not None:
-              status_text += "Z = %s\n" % status["z"]
-          if not status_text:
-            status_text = "Printer online"
-      except:
-        status_text = "Printer offline"
-      overlay.set_property("text", status_text.strip())
-      self.timer = threading.Timer(3.0, self.update_status)
-      self.timer.start()
+    def update_status(self):
+        try:
+            status = rpc.status()
+            status_text = ""
+            if status["filename"] is not None:
+                status_text += "Printing %s\n" % os.path.basename(status["filename"])
+            if status["eta"] is not None:
+                secondsremain, secondsestimate, progress = status["eta"]
+                status_text += "Est: %s of %s remaining\n" % (format_duration(secondsremain),
+                                                            format_duration(secondsestimate))
+            if status["temps"] is not None:
+                temps = status["temps"]
+                self.temp_log.append(temps)
+                if len(self.temp_log) > TEMP_LOG_SIZE:
+                    self.temp_log.pop(0)
+                graphsvg = make_temp_graph(self.temp_log)
+                graphoverlay.set_property("data", graphsvg)
+                #print >> sys.stderr, len(self.temp_log), graphsvg
+                status_text += "Temps: H: %s/%s, B: %s/%s\n" % (temps["T"][0], temps["T"][1], temps["B"][0], temps["B"][1])
+            if status["z"] is not None:
+                status_text += "Z = %s\n" % status["z"]
+            if not status_text:
+                status_text = "Printer online"
+        except:
+            status_text = "Printer offline"
+        overlay.set_property("text", status_text.strip())
+        self.timer = threading.Timer(3.0, self.update_status)
+        self.timer.start()
 
-  def stop(self):
-      self.timer.cancel()
+    def stop(self):
+        self.timer.cancel()
 
 statushandler = StatusHandler()
 
